121a83fa1316b51b51f92f5b6e831b13d008c736
ammup <ammup@ammu>
2024-04-01 15:20:25 +0530

    first

---
+++ corejava/src/corejava/sample.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int a=10,b=3;
+     boolean c;
+     c=true&&true&&true;
+     System.out.println(c);
+	}
+
+}
+
+
