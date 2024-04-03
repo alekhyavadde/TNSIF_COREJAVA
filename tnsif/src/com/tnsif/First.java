b3ccce359b6e37094f0a37cfc5b12f76ddad0529
ammup <ammup@ammu>
2024-04-03 15:09:27 +0530

    commit

--- tnsif/src/com/tnsif/First.java
+++ tnsif/src/com/tnsif/First.java
@@ -1,10 +1,10 @@
-package corejava;
+package com.tnsif;
 
-public class hello {
+public class First {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello");
+System.out.println("hello ammu");
 	}
 
 }
